c2e7e4e4773dd42fdcc683fb4a1f7eac25c1993c
Jon McCartie <jon@mccartie.com>
2012-12-18 13:54:52 -0800

    Initial commit of the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Focal::Application.config.secret_token = 'd36cdec764de672d67e7d00ce9b2fae5b0c6380f7ec4e8ae51fe036b438c9d493e455a1bcf14d770914516f849726c534415d7a7f8b45e0e0b4346ca29fdfea7'
